25a28a55dd766625c581d8eeb3c51801d12deed6
Breno001a <brenohenriqueroquegomes@gmail.com>
2025-04-08 12:22:51 -0300

    080425

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,25 @@
+import Boasvindas from "./exemplo1"
+
+
+function App() {
+    <div>
+
+<h1>boas vindas ao React com JavaScript</h1>
+
+
+<p>Este e o arquivo app.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação React utilizando JavaScript.</p>
+
+
+<Boasvindas nome="Guilerme"/>
+<Boasvindas nome="Breno"/>
+<Boasvindas nome="João"/>
+<Boasvindas nome="Bigode gay"/>
+<Boasvindas nome="Pedro"/>
+</div>
+
+  return (
+
+  )
+}
+
+export default App
---
+++ boasvindas/src/exemplo1.jsx
@@ -0,0 +1,12 @@
+export default function Boasvindas ({nome})
+{
+    return (
+        <div>
+            <p>
+                Olá {nome} seja bem vindo ao React. 
+            </p>
+
+        </div>
+      
+    );
+}
