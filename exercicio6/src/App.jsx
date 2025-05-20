338f0c49636e1c8a080e3ffa1fe5a7c1a1eaf6fa
Breno171 <brenohenriqueroquegomes@gmail.com>
2025-05-20 12:29:50 -0300

    20/05

---
+++ exercicio6/src/App.jsx
@@ -0,0 +1,12 @@
+import Exercicio3 from "../../exercicioscomponentes/src/Componentes/Exercicio4";
+import exercicio6 from "./Exercicio6";
+
+export default function App()
+{
+  return (
+    <div>
+      <h1>Exercicio 6</h1>
+      <exercicio6 quantidade={2} preco={5}/>
+    </div>
+  );
+}
---
+++ exercicio6/src/Exercicio6.jsx
@@ -0,0 +1,16 @@
+export default function exercicio6 ({quantiadade, preco})
+{
+    let subtotal= (quantiadade, preço)
+    let desconto= (subtotal*0.1)
+    let valorf= (subtotal-desconto)
+
+    return (
+
+    <div>
+       Total: {subtotal} </br>
+       Desconto: R${desconto} </br>
+       Valor Final: R${valorf} </br>
+    </div>  
+
+    );
+    }  
